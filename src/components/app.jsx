715d6e5f4a5974c13a8532dff6f2a524af171896
Jasmine Dang <ngandth.1992@gmail.com>
2020-07-26 16:08:25 +0800

    fix syntax error scss

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const App = (props) => {
   return (
     <div className='container'>
-      <div className='flat-list'></div>
+      <div className='flat-list'>
+        <Flat />
+      </div>
       <div className='map-container'></div>
     </div>
   );
