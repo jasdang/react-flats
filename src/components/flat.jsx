4fc6839826b09a18bbcc66cb5ee63b0b878fdc06
Jasmine Dang <ngandth.1992@gmail.com>
2020-07-26 17:04:42 +0800

    refactor Flat and FlatList

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,31 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Flat extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: this.props.name,
-      imageUrl: this.props.imageUrl,
-      price: this.props.price,
-      priceCurrency: this.props.priceCurrency,
-      lat: this.props.lat,
-      lng: this.props.lng,
-    };
-  }
-  render() {
-    const divStyle = {
-      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${this.state.imageUrl})`,
-    };
-    return (
-      <div className='card' style={divStyle}>
-        <div className='card-category'>{`${this.state.price} ${this.state.priceCurrency}`}</div>
-        <div className='card-description'>
-          <h2>{this.state.name}</h2>
-        </div>
-        <a className='card-link' href='#'></a>
+const Flat = ({name, imageUrl, price, priceCurrency, lat, lng}) => {
+  const divStyle = {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${imageUrl})`,
+  };
+  return (
+    <div className='card' style={divStyle}>
+      <div className='card-category'>{`${price} ${priceCurrency}`}</div>
+      <div className='card-description'>
+        <h2>{name}</h2>
       </div>
-    );
-  }
-}
+      <a className='card-link' href='#'></a>
+    </div>
+  );
+};
 
 export default Flat;
