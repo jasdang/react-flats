99f13339173112aa3cd2f8fca0ed132c84f6c1bf
Jasmine Dang <ngandth.1992@gmail.com>
2020-07-26 21:54:32 +0800

    transform App to class based component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import FlatList from './flat_list';
 import SimpleMap from './map';
 
-const App = (props) => {
-  return (
-    <div className='container'>
-      <div className='flat-list'>
-        <FlatList />
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='flat-list'>
+          <FlatList />
+        </div>
+        <div className='map-container'>
+          <SimpleMap lat='55' lng='55' />
+        </div>
       </div>
-      <div className='map-container'>
-        <SimpleMap lat='55' lng='55' />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
