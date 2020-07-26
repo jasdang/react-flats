7fdeecb10a503d4a74049cb2854ec59138673fbc
Jasmine Dang <ngandth.1992@gmail.com>
2020-07-26 22:07:58 +0800

    created selectFlat in App and pass down to Flat

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,14 +3,34 @@ import FlatList from './flat_list';
 import SimpleMap from './map';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFlat: {
+        lat: 55,
+        lng: 55,
+      },
+    };
+  }
+
+  selectFlat = (event) => {
+    console.log(event.currentTarget);
+    // this.state({
+    //   selectedFlat: flat,
+    // });
+  };
+
   render() {
     return (
       <div className='container'>
         <div className='flat-list'>
-          <FlatList />
+          <FlatList selectFlat={this.selectFlat} />
         </div>
         <div className='map-container'>
-          <SimpleMap lat='55' lng='55' />
+          <SimpleMap
+            lat={this.state.selectedFlat.lat}
+            lng={this.state.selectedFlat.lng}
+          />
         </div>
       </div>
     );
--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -3,19 +3,22 @@ import Flat from './flat';
 import flats from '../../data/flats';
 
 const FlatList = (props) => {
-  return flats.map(({name, imageUrl, price, priceCurrency, lat, lng}) => {
-    return (
-      <Flat
-        name={name}
-        imageUrl={imageUrl}
-        price={price}
-        priceCurrency={priceCurrency}
-        lat={lat}
-        lng={lng}
-        key={`${lat}-${lng}`}
-      />
-    );
-  });
+  return flats.map(
+    ({name, imageUrl, price, priceCurrency, lat, lng, selectFlat}) => {
+      return (
+        <Flat
+          name={name}
+          imageUrl={imageUrl}
+          price={price}
+          priceCurrency={priceCurrency}
+          lat={lat}
+          lng={lng}
+          key={`${lat}-${lng}`}
+          selectFlat={selectFlat}
+        />
+      );
+    }
+  );
 };
 
 export default FlatList;
