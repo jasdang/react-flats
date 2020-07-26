3250f177bdd225474b8678c4d43d03678598cfa9
Jasmine Dang <ngandth.1992@gmail.com>
2020-07-26 16:39:31 +0800

    change html in Flat

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Flat from './flat';
 
 const App = (props) => {
   return (
--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -13,15 +13,17 @@ class Flat extends Component {
     };
   }
   render() {
+    const divStyle = {
+      backgroundImage:
+        'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url({this.state.imageUrl})',
+    };
     return (
-      <div
-        class='card'
-        style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';);">
-        <div class='card-category'>150 EUR</div>
-        <div class='card-description'>
+      <div className='card' style={divStyle}>
+        <div className='card-category'>{`${this.state.price} ${this.state.priceCurrency}`}</div>
+        <div className='card-description'>
           <h2>Super 60m2 in trendy neighborhood!</h2>
         </div>
-        <a class='card-link' href='#'></a>
+        <a className='card-link' href='#'></a>
       </div>
     );
   }
